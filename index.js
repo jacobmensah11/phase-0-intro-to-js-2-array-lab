6d65b6b9dd1bccb66a7217a3a704fe45222475d2
jacobmensah11 <jacob.mensah@student.moringaschool.com>
2022-05-25 23:02:39 +0000

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution here!
+function writeCards(arr,eventName){
+    const newArr = []
+    for(let i=0;i<arr.length;i++){
+
+      newArr[i] = `Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`;
+
+    }
+    return newArr;
+  }
+
+
+  function countDown(num){
+    let i=num;
+    while( i >= 0){
+      console.log(i);
+      i--
+    }
+  } 
